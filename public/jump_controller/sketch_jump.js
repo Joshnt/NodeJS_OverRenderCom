27716bbbb350d0b2febf20082d35bf44c6286f9f
Joshn't <86410587+Joshnt@users.noreply.github.com>
2025-04-16 00:06:44 +0200

    Reverted Mobile check (buggy)

--- public/jump_controller/sketch_jump.js
+++ public/jump_controller/sketch_jump.js
@@ -7,19 +7,6 @@ function setup() {
   // Connect to your remote server on Render.
   // Change "your-app-name.onrender.com" to your actual Render domain.
   socket = io.connect("https://nodejs-unity.onrender.com");
-
-  // copied from ronikaufman
-  /* Storing user's device details in a variable*/
-  let details = navigator.userAgent;
-
-  /* Creating a regular expression 
-  containing some mobile devices keywords 
-  to search it in details string*/
-  let regexp = /android|iphone|kindle|ipad/i;
-
-  /* Using test() method to search regexp in details
-  it returns boolean value*/
-  let isMobileDevice = regexp.test(details);
 }
 
 function draw() {
